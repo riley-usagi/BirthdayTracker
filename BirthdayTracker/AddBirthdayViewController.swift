15e23fd3c9bd46f1e4fec8d331cb658d802b3e38
Riley Usagi <riley.usagi@gmail.com>
2018-04-29 08:31:03 +0300

    Inputs

--- BirthdayTracker/AddBirthdayViewController.swift
+++ BirthdayTracker/AddBirthdayViewController.swift
@@ -2,13 +2,24 @@ import UIKit
 
 class AddBirthdayViewController: UIViewController {
 
-  // Elements
+  // Элементы интерфейса
   @IBOutlet var firstNameTextField: UITextField!
   @IBOutlet var lastNameTextField: UITextField!
   @IBOutlet var birthdayPicker: UIDatePicker!
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    // Человек не может родиться в будущем
+    birthdayPicker.maximumDate = Date()
+  }
+  
+  @IBAction func saveTapped(_ sender: UIBarButtonItem) {
+    print("Нажата кнопка сохранения")
+    
+    let firstName = firstNameTextField.text ?? ""
+    let lastName = lastNameTextField.text ?? ""
+    print("Привет! Меня зовут \(firstName) \(lastName).")
   }
 }
 
