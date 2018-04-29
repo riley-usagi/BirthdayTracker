cead237087f751f70d7731c3dfd29027ee617d1c
Riley Usagi <riley.usagi@gmail.com>
2018-04-29 08:19:30 +0300

    Outlets

---
+++ BirthdayTracker/AddBirthdayViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class AddBirthdayViewController: UIViewController {
+
+  // Elements
+  @IBOutlet var firstNameTextField: UITextField!
+  @IBOutlet var lastNameTextField: UITextField!
+  @IBOutlet var birthdayPicker: UIDatePicker!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
--- BirthdayTracker/Birthday.swift
+++ BirthdayTracker/Birthday.swift
@@ -1,1 +1,13 @@
 import Foundation
+
+class Birthday {
+  let firstName: String
+  let lastName: String
+  let birthDate: Date
+  
+  init(firstName: String, lastName: String, birthDate: Date) {
+    self.firstName = firstName
+    self.lastName = lastName
+    self.birthDate = birthDate
+  }
+}
